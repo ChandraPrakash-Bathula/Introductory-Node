1423c0b383486641826a3f37ad17fbaffdb47755
Ram Mohan <chandu270>
2021-01-22 18:20:50 +0530

    Introduction to Node

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+var mySuperVillainName = supervillains.random();
+
+console.log(mySuperheroName);
+console.log(mySuperVillainName);
